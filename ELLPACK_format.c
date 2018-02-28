32e5bc0b957697cd8c5fdff43dfe6e3ca00fa3eb
Augustin <a.reille@net.estia.fr>
2018-02-28 12:45:16 +0000

    ELLPACK ok

--- ELLPACK_format.c
+++ ELLPACK_format.c
@@ -56,6 +56,69 @@ int main(int argc, char *argv[])
                 MAXNZ = local_nz;
         }
     }
+    /**************************/
+
+    /***********************/
+    /* JA & AS CALCULATION */
+    /***********************/
+    int **JA = (int **)malloc(M * sizeof(int *));
+    int **AS = (int **)malloc(M * sizeof(int *));
+    for (int i = 0; i < M; i++)
+    {
+        JA[i] = (int *)malloc(MAXNZ * sizeof(int));
+        AS[i] = (int *)malloc(MAXNZ * sizeof(int));
+    }
+
+    for (i = 0; i < M; i++)
+    {
+        int index = 0;
+        int local_nz = 0;
+        for (j = 0; j < N; j++)
+        {
+            if (mat[i][j] != 0)
+            {
+                local_nz++;
+                if (index < MAXNZ)
+                {
+                    JA[i][index] = j;
+                    AS[i][index] = mat[i][j];
+                    index++;
+                    if (local_nz < MAXNZ && JA[i][index] == 0)
+                    {
+                        JA[i][index] = JA[i][index - 1];
+                    }
+                }
+            }
+        }
+    }
+
+    printf("MAXNZ = %d\n", MAXNZ);
+
+    /***************/
+    /* JA PRINTING */
+    /***************/
+    printf("JA = \n");
+    for (i = 0; i < M; i++)
+    {
+        for (j = 0; j < MAXNZ; j++)
+        {
+            printf("%d  ", JA[i][j]);
+        }
+        printf("\n");
+    }
+
+    /***************/
+    /* AS PRINTING */
+    /***************/
+    printf("AS = \n");
+    for (i = 0; i < M; i++)
+    {
+        for (j = 0; j < MAXNZ; j++)
+        {
+            printf("%d  ", AS[i][j]);
+        }
+        printf("\n");
+    }
 
     return 0;
 }
