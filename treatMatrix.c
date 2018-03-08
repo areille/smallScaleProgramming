d8f88a26735ff4f5d73cc0ee3706efa7fab9f1cf
Augustin <a.reille@net.estia.fr>
2018-03-08 11:48:31 +0000

    csr ok

--- treatMatrix.c
+++ treatMatrix.c
@@ -8,6 +8,8 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
 #include "mmio.h"
 
 int main(int argc, char *argv[])
@@ -18,16 +20,30 @@ int main(int argc, char *argv[])
     int M, N, nz;
     int i, *I, *J;
     double *val;
+    bool isCsrFormat;
 
-    if (argc < 2)
+    if (argc < 3)
     {
-        fprintf(stderr, "Usage: %s [martix-market-filename]\n", argv[0]);
+        fprintf(stderr, "Usage: %s [martix-market-filename] [1 for CSR, 2 for Ellpack]\n", argv[0]);
         exit(1);
     }
     else
     {
         if ((f = fopen(argv[1], "r")) == NULL)
             exit(1);
+        if (strcmp(argv[2], "1") == 0)
+        {
+            isCsrFormat = true;
+        }
+        else if (strcmp(argv[2], "2") == 0)
+        {
+            isCsrFormat = false;
+        }
+        else
+        {
+            printf("Second argument should be 1 for CSR or 2 for ELLPACK\n");
+            exit(1);
+        }
     }
 
     if (mm_read_banner(f, &matcode) != 0)
@@ -72,21 +88,101 @@ int main(int argc, char *argv[])
     if (f != stdin)
         fclose(f);
 
-    // /************************/
-    // /* now write out matrix */
-    // /************************/
+    /************************/
+    /* now write out matrix */
+    /************************/
 
-    // mm_write_banner(stdout, matcode);
-    // mm_write_mtx_crd_size(stdout, M, N, nz);
-    // for (i = 0; i < nz; i++)
-    //     fprintf(stdout, "%d %d %20.19g\n", I[i] + 1, J[i] + 1, val[i]);
+    mm_write_banner(stdout, matcode);
+    mm_write_mtx_crd_size(stdout, M, N, nz);
+    for (i = 0; i < nz; i++)
+        fprintf(stdout, "%d %d %20.19g\n", I[i], J[i], val[i]);
+    printf("Columns : %d, rows : %d, non-zeros : %d\n\n", M, N, nz);
 
-    printf("Matrix has %d rows, %d columns, and %d non-zeros values.\n", M, N, nz);
+    if (isCsrFormat == true)
+    {
+        /*************************/
+        /* CSR FORMAT CALCULATION*/
+        /*************************/
+        int *IRP = (int *)malloc((M + 1) * sizeof(int *));
+        // ASSUMING MATLAB FIRST COLUMN INDEXING
+        IRP[0] = 1;
+        int index = 0;
+        int local_row_nz = 1;
+        for (i = 0; i < nz; i++)
+        {
+            if (I[i] == I[i + 1])
+            {
+                local_row_nz++;
+            }
+            else
+            {
+                IRP[index + 1] = IRP[index] + local_row_nz;
+                local_row_nz = 1;
+                index++;
+            }
+        }
+        printf("---- CSR FORMAT RESULTS ----\n\n");
+        /****************/
+        /* IRP PRINTING */
+        /****************/
+        printf("IRP = [");
+        for (i = 0; i < M + 1; i++)
+        {
+            if (i < M)
+            {
+                printf("%d, ", IRP[i]);
+            }
+            else
+            {
+                printf("%d", IRP[i]);
+            }
+        }
+        printf("]\n");
+        /****************/
 
-    /*************************/
-    /* CSR FORMAT CALCULATION*/
-    /*************************/
-    // TODO
+        /****************/
+        /* JA PRINTING */
+        /****************/
+        printf("JA = [");
+        for (i = 0; i < nz; i++)
+        {
+            if (i < nz - 1)
+            {
+                printf("%d, ", J[i]+1);
+            }
+            else
+            {
+                printf("%d", J[i]+1);
+            }
+        }
+        printf("]\n");
+        /****************/
 
+        /****************/
+        /* AS PRINTING */
+        /****************/
+        printf("AS = [");
+        for (i = 0; i < nz; i++)
+        {
+            if (i < nz - 1)
+            {
+                printf("%3.2f, ", val[i]);
+            }
+            else
+            {
+                printf("%3.2f", val[i]);
+            }
+        }
+        printf("]\n");
+        /****************/
+        free(IRP);
+    }
+    else
+    {
+        /*************************/
+        /* ELLPACK FORMAT CALCULATION*/
+        /*************************/
+        printf("hello");
+    }
     return 0;
 }
